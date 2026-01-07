80e0f97f49e97b6c35944e0daaf99e10c77aa874
grzesiok <gregorkmail@gmail.com>
2026-01-07 21:05:35 +0100

    remove unused files

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/GIT/grzesiok/Chrupcio
-# Build directory: D:/GIT/grzesiok/Chrupcio/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
