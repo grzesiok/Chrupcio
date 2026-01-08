b8e5ae4fbe68e01ad7c06ff1d2e63ed87fd0377f
grzesiok <gregorkmail@gmail.com>
2026-01-08 10:18:33 +0100

    Async and Sync execution of commands

--- execute_unit/Command.h
+++ execute_unit/Command.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 
+#include <atomic>
+
 class CCommand
 {
 public:
@@ -12,7 +14,10 @@ public:
 	virtual ~CCommand(void) {
 	}
 
-	virtual uint32_t execute(void* pdata, size_t dataSize) = 0;
+	// Single execution entry point which optionally supports cancellation.
+	// Implementations that don't support cancellation may ignore the
+	// `cancelFlag` parameter (it defaults to nullptr).
+	virtual uint32_t execute(void* pdata, size_t dataSize, std::atomic<bool>* cancelFlag = nullptr) = 0;
 private:
 	std::string m_cmd;
 	std::string m_description;
